0ad33ac10eb2e18f31f06bc420a0dbc5d7a34bd9
Randall <randall@duomai.com>
2019-03-15 14:23:15 +0800

    rxjs序列操作

--- src/app/rxjs.demo.js
+++ src/app/rxjs.demo.js
@@ -2,22 +2,38 @@ const Rx = require('rxjs/Rx')
 /** 
  * Observable被称为可观察序列，简单来说数据就在Observable中流动，你可以使用各种operator对流处理，例如：
  */
-const ob = Rx.Observable.interval(1000)
-ob.take(3).map(n => n * 2).filter(n => n >= 2).subscribe({
-    next: n => console.log(n),
-    error: err => console.log(err),
-    complete: () => console.log('complete')
-}) 
-// subscribe中包含三个函数的参数对象被称为observer（观察者），表示的是对序列结果的处理方式。
-// next表示数据正常流动，没有出现异常；error表示流中出错，可能是运行出错，http报错等等；complete表示流结束，不再发射新的数据。
-// 其中只有next是必须的，其他两个方法是可选的，因此等同于.subscribe(n => console.log(n))
+// const ob = Rx.Observable.interval(1000)
+// ob.take(3).map(n => n * 2).filter(n => n >= 2).subscribe({
+//     next: n => console.log(n),
+//     error: err => console.log(err),
+//     complete: () => console.log('complete')
+// }) 
+
+// /**
+//  * 合并序列之Observable.concat() ： 
+//  * Observable 顺序的、串行的将所有输入 Observable 的值合并给输出 Observable。
+//  */
+// // exp1-1
+// var timer = Rx.Observable.interval(1000).take(4);
+// var sequence = Rx.Observable.range(1, 10);
+// var result = timer.concat(sequence);
+// result.subscribe(x => console.log(x));
+
+// // exp1-2
+// var timer1 = Rx.Observable.interval(1000).take(10)
+// var timer2 = Rx.Observable.interval(2000).take(6)
+// var timer3 = Rx.Observable.interval(500).take(10)
+// var result = timer1.concat(timer2, timer3)
+// result.subscribe(x => console.log(x))
 
 /**
- * 创建可观察序列的几种方式
+ * 合并序列之Observable.merge() ：
+ * 所有的输入 Observable 都完成了，输出 Observable 才能完成。该 Observable 发出的项是每个输入 Observable 的结果。 
  */
-Observable.of(...args) // Observable.of()可以将普通Javascript数据转为可观察序列。
-Observable.fromPromise(promise) // 将promise转换为Observable
-Observable.fromEvent(element, eventName) // 从DOM是事件创建序列，例如Observable.fromEvent($input, 'click')
-Observable.ajax(url | AjaxRequest) // 发送http请求
-
+// exp2-1
+var timer1 = Rx.Observable.interval(1000).take(10);
+var timer2 = Rx.Observable.interval(2000).take(6);
+var timer3 = Rx.Observable.interval(500).take(10);
+var merged = timer1.merge(timer2, timer3);
+merged.subscribe(x => console.log(x));
 
