f5fe5e65120ca5eb3e63bb1b80f566d0d6fb65e9
Randall <randall@duomai.com>
2019-03-15 16:03:29 +0800

    接口的可选属性、任意属性、只读属性

--- src/app/practice/ts-demo.ts
+++ src/app/practice/ts-demo.ts
@@ -11,3 +11,62 @@ let tom: Person = {
     name: 'Tom',
     age: 25,
 } // 定义的变量的属性必须和接口一致，多一些、少一些都不可以
+
+/**
+ * 接口的可选属性:
+ * 有时不希望完全匹配一个形状，即可以使用可选属性
+ */
+interface Police {
+    car: boolean
+    gun?: boolean
+}
+
+let bob: Police = {
+    car: true,
+}
+
+/**
+ * 接口的任意属性：
+ */
+interface Student {
+    name: string
+    age: number
+    [propName: string]: any 
+}
+
+let alice: Student = {
+    name: 'Alice',
+    age: 12,
+    gender: 'female'
+}
+
+// 一旦定义了任意属性，那么确定属性和可选属性都必须是它的类型的在子集： 下面会报错
+
+// interface Teacher {
+//     name: string
+//     age: number
+//     [propName: string]: string 
+// }
+
+// let ryan: Teacher = {
+//     name: 'Ryan',
+//     age: 34,
+//     gender: 'male'
+// }
+
+/**
+ * 只读属性————该属性只能在创建时被赋值：
+ */
+interface Gun {
+    readonly id: number
+    name: string
+    producer: string
+}
+
+let ak47: Gun = {
+    id: 41,
+    name: 'AK47',
+    producer: 'RU'
+}
+
+// ak47.id = 43 // 不可修改只读属性，会报错
